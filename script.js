81d4ca82ad81b8fce29df3b98b49315728d0a32f
biodun1991 <66769485+biodun1991@users.noreply.github.com>
2024-01-06 17:35:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let loader = document.getElementById("preloader");
+
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
+
+//  email send
+
+//   (function () {
+//     emailjs.init("5Rrz1HCmtPGDZBWf7");
+//   })();
