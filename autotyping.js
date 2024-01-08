f7357c7b7aa4f04be772623a742030f4650f1751
biodun1991 <66769485+biodun1991@users.noreply.github.com>
2024-01-08 04:09:34 +0100

    Add files via upload

---
+++ autotyping.js
@@ -0,0 +1,9 @@
+let typed = new Typed('.animate-text', {
+	strings: ['Front-End Developer.', 'Web Designer.', 'UI/UX Designer.','Marketing Expert.'],
+	typeSpeed: 60,
+	backSpeed: 40,
+	loop: true,
+	loopCount: Infinity,
+	startDelay:500,
+	
+});
