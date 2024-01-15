7267e85a2b8061cb6debf3f87498cf77f4108711
biodun1991 <66769485+biodun1991@users.noreply.github.com>
2024-01-15 03:08:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ window.addEventListener("load", function () {
 //   (function () {
 //     emailjs.init("5Rrz1HCmtPGDZBWf7");
 //   })();
+
+// code that hide the menu list when any of the link is clicked
+
+const listItem = document.querySelectorAll(".list-items");
+const buttonItem = document.querySelector(".button-div .navbar-toggler"); // Correct the selector
+const menu = document.querySelector(".d-menu"); // Correct the selector
+
+if (buttonItem && menu) {
+  for (let i = 0; i < listItem.length; i++) {
+    listItem[i].addEventListener("click", function () {
+      if (window.innerWidth < 768) {
+        buttonItem.click();
+      }
+    });
+  }
+}
